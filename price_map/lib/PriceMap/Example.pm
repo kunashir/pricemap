a092117ff7cde6e7e4389a72622fdedffd2bb597
kunashir <kunashir@list.ru>
2012-08-01 23:33:18 +0400

    auth in code - ready

--- price_map/lib/PriceMap/Example.pm
+++ price_map/lib/PriceMap/Example.pm
@@ -4,7 +4,8 @@ use Mojo::Base 'Mojolicious::Controller';
 # This action will render a template
 sub welcome {
   my $self = shift;
-
+  
+  return unless $self->digest_auth(allow => {sshaw => 'mu_pass'});
   # Render template "example/welcome.html.ep" with message
   $self->render(
     message => 'Welcome to the Mojolicious real-time web framework!');
